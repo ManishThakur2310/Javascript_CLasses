5d9d7c2984367128c38c7d62bcf583abc8701a88
ManishThakur2310 <167675165+ManishThakur2310@users.noreply.github.com>
2025-08-30 16:59:38 +0530

    Add files via upload

---
+++ Class 6_js/script.js
@@ -0,0 +1,13 @@
+ let arry = [3,5,7,9,2,4,1]
+for(let i = 0;i<arry.length;i++){
+    if(arry[i]==1){
+        console.log("index",i);
+    }
+}
+console.log(sizeOf(arry));
+
+let arr = []
+arr[0] = 6;
+arr[5] = 45;
+console.log(arr[1]);
+
