d854a610aaf132e6bfe8e6ec6a72df4fc578635b
ManishThakur2310 <167675165+ManishThakur2310@users.noreply.github.com>
2025-08-30 17:01:32 +0530

    Add files via upload

---
+++ Class5_js/script.js
@@ -0,0 +1,102 @@
+// let obj1 = {
+//     C_name: "Dj_bhoi",
+//     sports: "kabbaddi",
+//     Age: 21,
+//     Er_number: 23
+// }
+
+// // console.log(obj1.C_name, obj1.Age);
+
+// let obj2 = {
+//     "C name": "Dj_bhoi",
+//     sports: "kabbaddi",
+//     Age: 21,
+//     "Er-number": 23
+// }
+
+// // console.log(obj2["C name"], obj1.Age);
+
+// let obj3 = {
+//     "C name": "Dj_bhoi",
+//     "sports" : "kabbaddi",
+//     "Age" : 21,
+//     "Er-number": 23,
+//     isPlaying : function () {
+//         console.log("yes");
+//     },
+//     inner_obj : {
+//         "national" : "played" ,
+//         "currently playing" : "yes",
+//         123 : "hello"
+//     }
+// }
+
+// // console.log(obj3.inner_obj["123"]);
+// // console.log(obj1,obj2,obj3);
+
+// let obj4 = {
+//     "C name": "Dj",
+//     "sports" : "kabbaddi",
+//     "Age" : 21,
+//     "Er-number": 23,
+//     isPlaying : function () {
+//         console.log("yes");
+//     },
+//     inner_obj1 : {
+//         "national" : "played" ,
+//         "currently playing" : "yes",
+//         123 : "hello",
+//         "Not a function" : function() {
+//             obj5 = {
+//                 other_name : "Manish",
+//                 "other_age" : 25
+//             }
+//             return obj5.other_age
+//         }
+//     }
+// }
+
+// let x = obj4.inner_obj1["Not a function"]()
+// console.log(x);
+
+// let today = new Date();
+// console.log(today);
+
+// arr = [0,1,2,3]
+// console.log(arr[arr.length-1]);
+
+// arr = [{ "name": "Hello", id : 1515},55,"Kishan"]
+
+// console.log(arr[1],arr[0].name);
+// console.log(arr[arr.length-1]);
+
+// obj1 = {
+//     id : 1515,
+//     cid:1414
+// }
+// // obj1 = {
+// //     sid:1616
+// // }
+
+// console.log(obj1);
+
+// if(id in obj1){
+//     console.log("yes");
+// }
+
+
+// let arry = [3,5,7,9,2,4,1]
+// for(let i = 0;i<arry.length;i++){
+//     if(arry[i]==1){
+//         console.log("index",i);
+//     }
+// }
+// console.log(sizeOf(arry));
+
+// let arr = []
+// arr[0] = 6;
+// arr[5] = 45;
+// console.log(arr[1]);
+
+
+
