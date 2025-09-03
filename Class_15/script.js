16bb3a9ca8e7484bf45b568b0f25b86cb7ef4fb7
ManishThakur2310 <167675165+ManishThakur2310@users.noreply.github.com>
2025-09-03 17:14:52 +0530

    Dom

---
+++ Class_15/script.js
@@ -0,0 +1,56 @@
+// let obj = {
+//     c_name : "Manish",
+//     data : {
+//         c_id : 101
+
+//     }
+// }
+
+// console.log("hey");
+// {
+//   let a = 1;
+//   let b = 2;
+//   console.log(a);
+//   console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+
+// console.log("hey");
+// {
+//   var a = 1;
+//   var b = 2;
+//   console.log(a);
+//   console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+
+// console.log(a);
+//  var a=()=>{
+//      console.log("hello");
+//      }
+
+
+// console.log(a);
+// function a(){
+//     console.log("hello");
+// }
+
+
+let ele = document.getElementById("head1")
+console.log(ele);
+
+let clss = document.getElementsByClassName("xyz")
+console.log(clss);
+
+let tag = document.getElementsByTagName("div")
+console.log(tag);
+
+let query1 = document.querySelector("div")
+console.log(query1);
+
+let queryall = document.querySelectorAll("div")
+console.log(queryall);
